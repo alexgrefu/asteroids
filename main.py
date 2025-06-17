4704b4469fdf313cce5a32bd4f6d42ecf0088b76
Alex Grefu <alex.grefu@gmail.com>
2025-06-17 18:50:33 +0300

    added player implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   print("Starting Asteroids!")
@@ -8,14 +9,30 @@ def main():
 
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+  clock = pygame.time.Clock()
+  dt = 0 # delta time
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   while True:
-    
+
+    dt = clock.tick(60) / 1000
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
-      
-    screen.fill((0, 0, 0))
+    
+    # clear the screen
+    screen.fill("black")
+    
+    player.draw(screen)
+    player.update(dt)
+
+    # update the display
     pygame.display.flip()
+
+    
+
 
 if __name__ == "__main__":
   main()
